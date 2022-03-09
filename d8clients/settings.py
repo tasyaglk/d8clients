209b1c551717860c6aab64a3d6bc1a9b67f484f3
Taisia Galkina <galkina.tasya@mail.ru>
2022-03-09 15:33:11 +0300

    change db path for docker

--- d8clients/settings.py
+++ d8clients/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'd8clients.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/db/sqlite.db',
     }
 }
 
